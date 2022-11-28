99b45231f008c1a328bb6e78f68349005977922d
RAguirreGiraldo <raguirregiraldo@gmail.com>
2022-11-28 15:54:15 -0600

    update index, html and js

--- assets/script/index.js
+++ assets/script/index.js
@@ -55,15 +55,3 @@ function newContact(){
 }
 
 
-function setup() {
-    document.getElementById('buttonid').addEventListener('click', openDialog);
-    function openDialog() {
-        document.getElementById('fileid').click();
-    }
-    document.getElementById('fileid').addEventListener('change', submitForm);
-    function submitForm() {
-        document.getElementById('formid').submit();
-    }
-}
-
-
