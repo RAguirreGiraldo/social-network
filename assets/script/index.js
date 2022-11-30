6975dd5ecda1c5e81b64c0c23bf79630c7120a20
RAguirreGiraldo <raguirregiraldo@gmail.com>
2022-11-30 07:37:25 -0600

    update index.js index.css

--- assets/script/index.js
+++ assets/script/index.js
@@ -13,15 +13,18 @@ function newUser(){
 }
 
 function newPost(){
+    let actualUser = new User(1,"Roberto","RobertoUser","roberto@gm.com");
+    let data = actualUser.getInfo();
+    // Insert my id and image in control
     let postText = document.getElementById('entrytext').value;
     let postImg = document.getElementById('imgpath').value;
-    console.log("texto: " + postImg);
-    // clone this structure for the new post 
+    // clone this structure for the new post
     let postTemplate = document.getElementById("posttmpl");
     let clonedPost = postTemplate.cloneNode(true);
-    clonedPost.removeAttribute("hidden");
+    clonedPost.removeAttribute("hidden");    
     document.getElementById("postZone").appendChild(clonedPost);
     document.getElementById("userposted").innerHTML= data[1];
+    document.getElementById("post-text").innerHTML=   document.getElementById("entrytext").value;
 }
 
 
