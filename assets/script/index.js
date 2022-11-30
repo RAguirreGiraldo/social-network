9829f994fa01853b5f09b35f6ce4c61f173a60f1
RAguirreGiraldo <raguirregiraldo@gmail.com>
2022-11-30 07:23:53 -0600

    update

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,25 +1,27 @@
 'use strict';
 
 import User from "./User.js";
+
 newUser();
-//document.getElementById('submit-btn').addEventListener("click", newUser);
+document.getElementById('submit-btn').addEventListener("click", newPost);
+
 
 function newUser(){
     let actualUser = new User(1,"Roberto","RobertoUser","roberto@gm.com");
     let data = actualUser.getInfo();
-    console.log(data);
-    //id
+    //console.log(data);    
+}
+
+function newPost(){
+    let postText = document.getElementById('entrytext').value;
+    let postImg = document.getElementById('imgpath').value;
+    console.log("texto: " + postImg);
+    // clone this structure for the new post 
+    let postTemplate = document.getElementById("posttmpl");
+    let clonedPost = postTemplate.cloneNode(true);
+    clonedPost.removeAttribute("hidden");
+    document.getElementById("postZone").appendChild(clonedPost);
     document.getElementById("userposted").innerHTML= data[1];
-    
 }
 
 
-/*id
-document.getElementById("user").innerHTML= JSON.stringify(x);
-//name
-document.getElementById("user").innerHTML= x.name;
-//username
-document.getElementById("user").innerHTML= x.userName;
-//email
-document.getElementById("user").innerHTML= x.email;
-*/
